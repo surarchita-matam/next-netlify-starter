a1841af9e90c64e239ab908b7e0c58c47933aee8
surarchita-matam <47653849+surarchita-matam@users.noreply.github.com>
2023-06-07 14:02:38 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,23 +4,44 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+<div className="container">
+  <head>
+    <title>Next.js Starter!</title>
+    <link rel="icon" href="/favicon.ico" />
+  </head>
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
+  <main>
+    <header title="Welcome to my app!" />
+    <p className="description">Get started by editing <code>pages/index.js</code></p>
+  </main>
 
-      <Footer />
-        <script type="text/javascript">
-    window.ymConfig={"bot":"x1665474087516","host":"https://alpha9.yellowmessenger.com","payload":{}};(function () {var w=window,ic=w.YellowMessenger;if("function"===typeof ic)ic("reattach_activator"),ic("update",ymConfig);else{var d=document,i=function(){i.c(arguments)};function l(){var e=d.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://cdn.yellowmessenger.com/plugin/widget-v2/latest/dist/main.min.js";var t=d.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)}i.q=[],i.c=function(e){i.q.push(e)},w.YellowMessenger=i,l()}})();
+  <footer />
+  <script type="text/javascript">
+    window.ymConfig = { bot: "x1665474087516", host: "https://alpha9.yellowmessenger.com", payload: {} }
+    ;(function () {
+      var w = window,
+        ic = w.YellowMessenger
+      if ("function" === typeof ic) ic("reattach_activator"), ic("update", ymConfig)
+      else {
+        var d = document,
+          i = function () {
+            i.c(arguments)
+          }
+        function l() {
+          var e = d.createElement("script")
+          ;(e.type = "text/javascript"), (e.async = !0), (e.src = "https://cdn.yellowmessenger.com/plugin/widget-v2/latest/dist/main.min.js")
+          var t = d.getElementsByTagName("script")[0]
+          t.parentNode.insertBefore(e, t)
+        }
+        ;(i.q = []),
+          (i.c = function (e) {
+            i.q.push(e)
+          }),
+          (w.YellowMessenger = i),
+          l()
+      }
+    })()
   </script>
-    </div>
+</div>
   )
 }
