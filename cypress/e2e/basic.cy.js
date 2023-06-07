c6a03733701af0ed5ff247180493e91c142b4967
surarchita-matam <47653849+surarchita-matam@users.noreply.github.com>
2023-06-07 14:24:51 +0530

    Update basic.cy.js

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -7,11 +7,4 @@ describe('sample test', () => {
     cy.get('h1')
     .contains('Welcome to my app!');
   })
-  it('renders the Netlify logo image', () => {
-    cy.get('img')
-    .should('be.visible')
-    .and(($img) => {
-      expect($img[0].naturalWidth).to.be.greaterThan(0);
-    })
-  })
 })
