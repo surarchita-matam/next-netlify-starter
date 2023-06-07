225d4ec9f82035953bd573487d07a9d1fc51b074
surarchita-matam <47653849+surarchita-matam@users.noreply.github.com>
2023-06-07 14:10:02 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -19,18 +19,18 @@ export default function Home() {
   <script type="text/javascript">
     window.ymConfig = {{ "bot": "x1665474087516", "host": "https://alpha9.yellowmessenger.com", "payload": {} }}
     ;(function () {
-      var w = window,
+      let w = window,
         ic = w.YellowMessenger
       if ("function" === typeof ic) ic("reattach_activator"), ic("update", ymConfig)
       else {
-        var d = document,
+        let d = document,
           i = function () {
             i.c(arguments)
           }
         function l() {
-          var e = d.createElement("script")
+          let e = d.createElement("script")
           ;(e.type = "text/javascript"), (e.async = !0), (e.src = "https://cdn.yellowmessenger.com/plugin/widget-v2/latest/dist/main.min.js")
-          var t = d.getElementsByTagName("script")[0]
+          let t = d.getElementsByTagName("script")[0]
           t.parentNode.insertBefore(e, t)
         }
         ;(i.q = []),
