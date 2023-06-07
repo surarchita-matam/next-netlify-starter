9b9cff4463eda315eb7126855fdc18882d6eaee2
surarchita-matam <47653849+surarchita-matam@users.noreply.github.com>
2023-06-07 13:58:22 +0530

    adding script

--- pages/index.js
+++ pages/index.js
@@ -18,9 +18,9 @@ export default function Home() {
       </main>
 
       <Footer />
-    </div>
-    <script type="text/javascript">
+        <script type="text/javascript">
     window.ymConfig={"bot":"x1665474087516","host":"https://alpha9.yellowmessenger.com","payload":{}};(function () {var w=window,ic=w.YellowMessenger;if("function"===typeof ic)ic("reattach_activator"),ic("update",ymConfig);else{var d=document,i=function(){i.c(arguments)};function l(){var e=d.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://cdn.yellowmessenger.com/plugin/widget-v2/latest/dist/main.min.js";var t=d.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)}i.q=[],i.c=function(e){i.q.push(e)},w.YellowMessenger=i,l()}})();
   </script>
+    </div>
   )
 }
